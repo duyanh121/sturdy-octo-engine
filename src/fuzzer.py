1df24486c2166daf5f5a95e2469df51119f355b6
duyanh121 <131868208+duyanh121@users.noreply.github.com>
2025-11-15 13:38:23 +0700

    update fuzzer output

--- src/fuzzer.py
+++ src/fuzzer.py
@@ -1,5 +1,5 @@
 from typing import List
-from src.runner import Runner, RunnerResult
+from src.runner import Runner, RunnerResult, RunnerStatus
 import time
 
 class Fuzzer:
@@ -8,6 +8,8 @@ class Fuzzer:
 
     def fuzz(self, trails: int = 10, verbose: bool = True) -> List[RunnerResult]:
         results = []
+        result_pass = 0
+        result_fail = 0
         for i in range(trails):
             if verbose:
                 print(f"Fuzzing trial {i + 1}/{trails}... ", end="", flush=True)
@@ -16,5 +18,13 @@ class Fuzzer:
 
             if verbose:
                 print(f"{result.status.value.upper()}")
+
+                if result.status == RunnerStatus.PASS:
+                    result_pass += 1
+                else:
+                    result_fail += 1
+
+        if verbose:
+            print(f"Fuzzing complete: {trails} trials, {result_pass} passes, {result_fail} fails.")
 
         return results
